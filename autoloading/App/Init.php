0f668b08518132fbbf4ff28f3657ad029ece3c1f
vipscodes <vipscodes@gmail.com>
2020-09-26 22:59:21 +0700

    Autoloading

--- autoloading/App/Init.php
+++ autoloading/App/Init.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 
 // require 'Produk/InfoProduk.php';
@@ -7,6 +7,6 @@
 // require 'Produk/Game.php';
 // require 'Produk/CetakInfoProduk.php';
 
-spl_autoload_register(function( $class ) {
-	require_once 'Produk/' . $class . '.php';
+spl_autoload_register(function ($class) {
+	require_once __DIR__ . '/Produk/' . $class . '.php';
 });
