0dd677849fdcf1a488f91fedd7792cf4bb27763a
vipscodes <vipscodes@gmail.com>
2020-09-26 22:43:46 +0700

    Autoloading

---
+++ autoloading/App/Produk/Produk.php
@@ -0,0 +1,91 @@
+<?php 
+
+abstract class Produk
+{
+    protected
+        $judul,
+        $penulis,
+        $penerbit,
+        $diskon = 0,
+        $harga;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+
+    public function setJudul($judul)
+    {
+        if (!is_string($judul)) {
+            throw new Exception("Judul harus string");
+        }
+        $this->judul = $judul;
+    }
+
+    public function getJudul()
+    {
+        return $this->judul;
+    }
+
+    public function getPenulis()
+    {
+        return $this->penulis;
+    }
+
+    public function setPenulis($penulis)
+    {
+        $this->penulis = $penulis;
+    }
+
+
+    public function getPenerbit()
+    {
+        return $this->penerbit;
+    }
+
+    public function setPenerbit($penerbit)
+    {
+        $this->penerbit = $penerbit;
+    }
+
+    public function setHarga($harga)
+    {
+        return $this->harga = $harga;
+    }
+
+    public function getHarga()
+    {
+        return $this->harga - ($this->harga * $this->diskon / 100);
+    }
+
+    public function setDiskon($diskon)
+    {
+        $this->diskon = $diskon;
+    }
+
+    public function getDiskon()
+    {
+        return $this->diskon;
+    }
+
+
+
+
+    public function sayHello()
+    {
+        return "Hello World";
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    abstract public function getInfo();
+
+    
+}
---
+++ autoloading/index.php
@@ -0,0 +1,16 @@
+<?php 
+require 'App/Init.php';
+
+$produk1 = new Komik("Naruto", "Masashi Kishomoto", "Shonen Jump", 30000, 100);
+
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk($produk1);
+$cetakProduk->tambahProduk($produk2);
+
+echo $cetakProduk->cetak();
+
+echo "<hr>";
+
+new Coba();
