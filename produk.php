6dd899b62339034b27da15562f7093129a675d22
vipscodes <vipscodes@gmail.com>
2020-09-26 20:43:05 +0700

    Property dan Method

--- produk.php
+++ produk.php
@@ -6,18 +6,10 @@
 
 class Produk
 {
-    public $judul,
-        $penulis,
-        $penerbit,
-        $harga;
-
-    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
-    {
-        $this->judul = $judul;
-        $this->penulis = $penulis;
-        $this->penerbit = $penerbit;
-        $this->harga = $harga;
-    }
+    public $judul = "judul",
+        $penulis = "penulis",
+        $penerbit = "penerbit",
+        $harga = 0;
 
     public function getLabel()
     {
@@ -25,10 +17,29 @@ class Produk
     }
 }
 
-$produk1 = new Produk("Naruto", "Mashashi Kishimoto", "Shonen Jump", 30000);
-$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
-$produk3 = new Produk("Dragon Ball");
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
 
-echo "Komik : " . $produk1->getLabel();
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// $produk2->tambahProperty = "test";
+// var_dump($produk2);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Mashashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+
+echo "Komik : " . $produk3->getLabel();
 echo "<br>";
-echo "Game : " . $produk2->getLabel();
+echo "Game : " . $produk4->getLabel();
