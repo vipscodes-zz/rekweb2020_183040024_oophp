19cc8acc27ad8c126a368d26b99ac822425b3b70
vipscodes <vipscodes@gmail.com>
2020-09-26 19:43:35 +0700

    Class dan Object

---
+++ coba.php
@@ -0,0 +1,10 @@
+<?php
+
+class Coba
+{
+}
+
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
---
+++ produk.php
@@ -0,0 +1,12 @@
+<?php
+
+// jualan produk
+// komik
+// game
+
+class Produk
+{
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
