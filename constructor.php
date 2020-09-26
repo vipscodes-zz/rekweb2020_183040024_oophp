de330978cfa8d69ed24c76f2e1623b4e37c426d1
vipscodes <vipscodes@gmail.com>
2020-09-26 20:44:10 +0700

    Constructor

---
+++ constructor.php
@@ -0,0 +1,33 @@
+<?php
+
+// jualan produk
+// komik
+// game
+
+class Produk
+{
+    public $judul,
+        $penulis,
+        $penerbit,
+        $harga;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+}
+
+$produk1 = new Produk("Naruto", "Mashashi Kishimoto", "Shonen Jump", 30000);
+$produk2 = new Produk("Uncharted", "Neil Druckmann", "Sony Computer", 250000);
+$produk3 = new Produk("Dragon Ball");
+echo "Komik : " . $produk1->getLabel();
+echo "<br>";
+echo "Game : " . $produk2->getLabel();
